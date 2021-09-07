c45a93aeae07457dd3bd82080de26afa6110661f
vlask <vlad.dondokov@gmail.com>
2021-09-07 23:07:08 +0300

    card class made

--- js/script.js
+++ js/script.js
@@ -1,4 +1,65 @@
 "use strict"
+
+class MenuCard {   //класс карточки меню 
+    constructor(src, alt, title, descr, price, parentSelector){
+        this.src = src;
+        this.alt = alt;
+        this.title = title;
+        this.descr = descr;
+        this.price = price; 
+        this.parentSelector = document.querySelector(parentSelector);
+        this.transfer = 27;
+    }
+
+    changeToUAH(){
+        this.price *= this.transfer; 
+    }
+    render(){
+        this.changeToUAH();
+        const element = document.createElement('div');
+        element.innerHTML = `
+        <div class="menu__item">
+            <img src=${this.src} alt=${this.alt}>
+            <h3 class="menu__item-subtitle">${this.title}"</h3>
+            <div class="menu__item-descr">${this.descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+            </div>
+        </div>
+        `;
+        this.parentSelector.append(element);
+    }
+
+}
+new MenuCard(
+    "img/tabs/vegy.jpg",
+    "vegy",
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    9,
+    ".menu .container"
+).render();
+
+new MenuCard(
+    "img/tabs/post.jpg",
+    "post",
+    'Меню "Постное"',
+    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+    14,
+    ".menu .container"
+).render();
+new MenuCard(
+    "img/tabs/elite.jpg",
+    "elite",
+    'Меню “Премиум”',
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    21,
+    ".menu .container"
+).render();
+
+
 window.addEventListener('DOMContentLoaded', ()=>{
     //tabs
     const tabs = document.querySelectorAll('.tabheader__item'),
@@ -122,11 +183,13 @@ window.addEventListener('DOMContentLoaded', ()=>{
     const modalTimerId = setTimeout(openModel, 5000); // открыть модальное окно через 5 секунд
 
     function showModalByScroll(){
-        if (window.pageYOffset + document.documentElement.clientHeight >= 
+        if (window.pageYOffset + document.documentElement.clientHeight >=  //Если число прокрутили до конца страницы
             document.documentElement.scrollHeight){
-                openModel();
-                window.removeEventListener('scroll', showModalByScroll);
+                openModel(); 
+                window.removeEventListener('scroll', showModalByScroll); //открыть окно и удалить событие
         }
     }
-    window.addEventListener('scroll', showModalByScroll);
+    window.addEventListener('scroll', showModalByScroll); //Если скролим то выполняем функцию
 });
+
+
